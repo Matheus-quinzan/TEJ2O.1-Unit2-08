77f9ac283ab8fe12745b191c944204ce2bc20222
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-10-03 13:03:37 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,41 +1,56 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matheus Quinzan
+ * Created on: Sep 2025
+ * This program Turns the all the RGB LED colours
 */
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    // This turns on a LED)
+    // turns on a red
     pins.digitalWritePin(DigitalPin.P14, 1) 
     basic.pause(1000)
+
+    // turn on blue
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+
+    // turn on green
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
+
+    // turn on red and blue
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+
+    // turn on green and blue
     pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+
+    // turn on red and green
     pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(1000)
+    // turn on red, blue and green
     pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
+
+    // turn off red, blue and green
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     basic.pause(1000)
+    
+    basic.showIcon(IconNames.Yes)
 })
